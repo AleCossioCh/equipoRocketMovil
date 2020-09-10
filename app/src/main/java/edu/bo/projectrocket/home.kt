7b47fa6d4e45d9e5eb746ad4ed8482f34fb87393
Raquel Montero <nabogaria@gmail.com>
2020-09-10 17:31:22 -0400

    win and home

--- app/src/main/java/edu/bo/projectrocket/home.kt
+++ app/src/main/java/edu/bo/projectrocket/home.kt
@@ -5,19 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val home_button = findViewById<Button>(R.id.home_button)
+        setContentView(R.layout.activity_home)
+
+        val win_button = findViewById<Button>(R.id.win_button)
 
 
 
-        home_button.setOnClickListener {
+        win_button.setOnClickListener {
             //get text from edittexts
 
             //intent to start activity
-            val intent = Intent(this@MainActivity, home::class.java)
+            val intent = Intent(this@home, win::class.java)
             startActivity(intent)
 
         }
