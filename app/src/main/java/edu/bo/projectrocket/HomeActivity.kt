39d7a05a99d515dce60eee74ab7150cebb23f000
Raquel Montero <nabogaria@gmail.com>
2020-10-08 18:46:47 -0400

    crated image activity

--- app/src/main/java/edu/bo/projectrocket/HomeActivity.kt
+++ app/src/main/java/edu/bo/projectrocket/HomeActivity.kt
@@ -1,11 +1,20 @@
 package edu.bo.projectrocket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        educacionVial.setOnClickListener{
+                val intent = Intent(this@HomeActivity, ImageQuestionActivity::class.java)
+                startActivity(intent)
+
+
+        }
     }
 }
---
+++ app/src/main/java/edu/bo/projectrocket/ImageQuestionActivity.kt
@@ -0,0 +1,31 @@
+package edu.bo.projectrocket
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import kotlinx.android.synthetic.main.activity_image_question.*
+
+class ImageQuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_question)
+
+        idopcionEstacion.setOnClickListener{
+            idopcionEstacion.setBackgroundColor(Color.parseColor("#F5F6FA"))
+            idopcionEstacion.setBackgroundResource(R.drawable.bg_btn_red)
+        }
+
+        //val idNextView = findViewById<ImageButton>(R.id.idNextView)
+        idNextView.setOnClickListener {
+            //get text from edittexts
+
+            //intent to start activity
+            val intent = Intent(this@ImageQuestionActivity, MainActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
