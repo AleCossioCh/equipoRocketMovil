4ea5edbbbbffc8719e0daf07513423066d88daca
Raquel Montero <nabogaria@gmail.com>
2020-10-13 20:10:44 -0400

    fixed background button

--- app/src/main/java/edu/bo/projectrocket/HomeActivity.kt
+++ app/src/main/java/edu/bo/projectrocket/HomeActivity.kt
@@ -14,6 +14,10 @@ class HomeActivity : AppCompatActivity() {
                 val intent = Intent(this@HomeActivity, ImageQuestionActivity::class.java)
                 startActivity(intent)
         }
+        normasdeCirculacion.setOnClickListener {
+            val intent = Intent(this@HomeActivity, TextQuestionActivity::class.java)
+            startActivity(intent)
+        }
         idcalendar.setOnClickListener{
             val intent = Intent(this@HomeActivity, CalendarActivity::class.java)
             startActivity(intent)
--- app/src/main/java/edu/bo/projectrocket/TextQuestionActivity.kt
+++ app/src/main/java/edu/bo/projectrocket/TextQuestionActivity.kt
@@ -3,6 +3,7 @@ package edu.bo.projectrocket
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_image_question.*
 import kotlinx.android.synthetic.main.activity_text_question.*
 
 class TextQuestionActivity : AppCompatActivity() {
@@ -13,23 +14,29 @@ class TextQuestionActivity : AppCompatActivity() {
 
 
         buttonAnswer1.setOnClickListener(){
-            buttonAnswer1.setBackgroundResource(R.color.colorButtonWrong)
-            buttonAnswer1.setTextColor(Color.parseColor("#FFFFFF"))
+           // buttonAnswer1.setBackgroundColor(Color.parseColor("#F5F6FA"))
+            buttonAnswer1.setBackgroundResource(R.drawable.bg_btn_red)
+            buttonAnswer1.setTextColor(Color.parseColor("#F5F6FA"))
         }
 
         buttonAnswer2.setOnClickListener(){
-            buttonAnswer2.setBackgroundResource(R.color.colorButtonRigth)
-            buttonAnswer1.setTextColor(Color.parseColor("#FFFFFF"))
+            buttonAnswer2.setBackgroundResource(R.drawable.bg_btn_primary)
+            buttonAnswer2.setTextColor(Color.parseColor("#F5F6FA"))
+
         }
 
         buttonAnswer3.setOnClickListener(){
-            buttonAnswer3.setBackgroundResource(R.color.colorButtonWrong)
-            buttonAnswer1.setTextColor(Color.parseColor("#FFFFFF"))
+            //buttonAnswer3.setBackgroundColor(Color.parseColor("#F5F6FA"))
+            buttonAnswer3.setBackgroundResource(R.drawable.bg_btn_red)
+            buttonAnswer3.setTextColor(Color.parseColor("#F5F6FA"))
+
         }
 
         buttonAnswer4.setOnClickListener(){
-            buttonAnswer4.setBackgroundResource(R.color.colorButtonWrong)
-            buttonAnswer1.setTextColor(Color.parseColor("#FFFFFF"))
+            //buttonAnswer4.setBackgroundColor(Color.parseColor("#F5F6FA"))
+            buttonAnswer4.setBackgroundResource(R.drawable.bg_btn_red)
+            buttonAnswer4.setTextColor(Color.parseColor("#F5F6FA"))
+
         }
     }
 }
