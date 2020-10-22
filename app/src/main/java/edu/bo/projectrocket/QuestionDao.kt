5822b28f5bb309276b83eab034e27df63db901e1
Raquel Montero <nabogaria@gmail.com>
2020-10-22 17:23:49 -0400

    done icon

--- app/src/main/java/edu/bo/projectrocket/QuestionDao.kt
+++ app/src/main/java/edu/bo/projectrocket/QuestionDao.kt
@@ -3,5 +3,9 @@ package edu.bo.projectrocket
 class QuestionDao(var idQ: String,var question:String) {
 
 
-    //fun getQestions()
+    fun getQestions(){
+        var lista= ArrayList<QuestionDao>()
+        lista.add(QuestionDao("123","Que señalizacion es esta"))
+
+    }
 }
