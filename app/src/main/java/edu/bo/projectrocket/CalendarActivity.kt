620a46697811dd1dadb6f282a6f8c55c611496b4
ale <ale7cosch@gmail.com>
2020-10-22 17:06:55 -0400

    btnSave on calendarView redirection

--- app/src/main/java/edu/bo/projectrocket/CalendarActivity.kt
+++ app/src/main/java/edu/bo/projectrocket/CalendarActivity.kt
@@ -1,6 +1,8 @@
 package edu.bo.projectrocket
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,6 +12,12 @@ class CalendarActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
 
+        val buttonSave : Button = findViewById(R.id.buttonSave)
+        buttonSave.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 
 }
